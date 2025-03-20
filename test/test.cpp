bd974be70d30d565c097296464c825ccad3ac55c
isba06 <www.ismail.com@mail.ru>
2025-03-20 20:00:31 +0300

    fix adjacency lists in tests

--- test/test.cpp
+++ test/test.cpp
@@ -15,13 +15,81 @@ TEST(GraphTest, VertexNotExist) {
 TEST(GraphTest, Example0) {
   Graph graph(5, 4, 4, {{0, {1, 4}}, 
                          {1, {0, 2, 3}}, 
-                         {2, {0, 1}}, 
+                         {2, {1}}, 
                          {3, {1}}, 
                          {4, {0}}});
 
   std::vector<int> expected = {1, 2, 3, 3, 0};
 
   auto dist = graph.distances();
+
+  ASSERT_EQ(expected.size(), dist.size());
+  for (size_t i = 0; i < expected.size(); ++i) {
+    ASSERT_EQ(expected[i], dist[i]);
+  }
+}
+
+TEST(GraphTest, Example1) {
+  Graph graph(5, 4, 4, {{0, {1, 4}}, 
+                         {1, {0, 2, 3}}, 
+                         {2, {1}}, 
+                         {3, {1}}, 
+                         {4, {0}}});
+
+  std::vector<int> expected = {2, 1, 2, 0, 3};
+
+  auto dist = graph.distances(3);
+
+  ASSERT_EQ(expected.size(), dist.size());
+  for (size_t i = 0; i < expected.size(); ++i) {
+    ASSERT_EQ(expected[i], dist[i]);
+  }
+}
+
+TEST(GraphTest, Example2) {
+  Graph graph(5, 4, 4, {{0, {1, 4}}, 
+                         {1, {0, 2, 3}}, 
+                         {2, {1}}, 
+                         {3, {1}}, 
+                         {4, {0}}});
+
+  std::vector<int> expected = {1, 0, 1, 1, 2};
+
+  auto dist = graph.distances(1);
+
+  ASSERT_EQ(expected.size(), dist.size());
+  for (size_t i = 0; i < expected.size(); ++i) {
+    ASSERT_EQ(expected[i], dist[i]);
+  }
+}
+
+TEST(GraphTest, Example3) {
+  Graph graph(5, 4, 4, {{0, {1, 4}}, 
+                         {1, {0, 2, 3}}, 
+                         {2, {1}}, 
+                         {3, {1}}, 
+                         {4, {0}}});
+
+  std::vector<int> expected = {0, 1, 2, 2, 1};
+
+  auto dist = graph.distances(0);
+
+  ASSERT_EQ(expected.size(), dist.size());
+  for (size_t i = 0; i < expected.size(); ++i) {
+    ASSERT_EQ(expected[i], dist[i]);
+  }
+}
+
+TEST(GraphTest, Example4) {
+  Graph graph(5, 4, 4, {{0, {1, 4}}, 
+                         {1, {0, 2, 3}}, 
+                         {2, {1}}, 
+                         {3, {1}}, 
+                         {4, {0}}});
+
+  std::vector<int> expected = {2, 1, 0, 2, 3};
+
+  auto dist = graph.distances(2);
 
   ASSERT_EQ(expected.size(), dist.size());
   for (size_t i = 0; i < expected.size(); ++i) {
