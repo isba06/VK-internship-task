e9ac447da03f0e462e3a6f27a03c308a8656eee9
isba06 <www.ismail.com@mail.ru>
2025-03-20 16:33:15 +0300

    first version

--- main.cpp
+++ main.cpp
@@ -2,12 +2,29 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <map>
+#include <queue>
 
 struct Graph {
     int node_num = 0;
     int edge_num = 0;
-    std::vector<std::pair<int, int>> edges;
+    std::map<int, std::vector<int>> adj_list;
+    Graph(const std::string & filename);
 };
+
+std::ostream& operator<<(std::ostream& os, const Graph& graph)
+{
+    os << "node num: " << graph.node_num << ", edge num: " << graph.edge_num << '\n';
+    os << "   Vertex FROM\t  " << "Verteces TO\n";
+    for(const auto & [vertex, verteces_list] : graph.adj_list) {
+        os << '\t' << vertex << "\t     ";
+        for(const auto & vertex2 : verteces_list) {
+            os << vertex2 << ' ';
+        }
+        os << '\n';
+    }
+    return os;
+}
 
 int main()
 {
@@ -26,11 +43,32 @@ int main()
 
     while(file >> first_edge) {
         if(file >> second_edge) {
-            graph.edges.push_back({first_edge, second_edge});
+            graph.adj_list[first_edge].push_back(second_edge);
+            graph.adj_list[second_edge].push_back(first_edge);
         } else {
             vertex_from = first_edge;
         }
     }
-    
+    file.close();
+
+    std::queue<int> q;
+    std::vector<int> dist(graph.node_num, INT_MAX);
+    dist[vertex_from] = 0;
+    q.push(vertex_from);
+
+    while(!q.empty()) {
+        int v = q.front();
+        q.pop();
+        for (const auto & u : graph.adj_list[v]) {
+            if (dist[u] > dist[v] + 1) {
+                dist[u] = dist[v] + 1;
+                q.push(u);
+            }
+        }
+    }
+
+    for(const auto & d : dist) {
+        std::cout << d << '\n';
+    }
     return 0;
 }
